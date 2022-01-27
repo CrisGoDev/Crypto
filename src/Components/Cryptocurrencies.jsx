b18dcc38139b3f0af775a3638f0433c339429eb2
Crisjar3 <joelchupapapaya@gmail.com>
2022-01-26 23:40:04 -0600

    Fixing new BUgs

--- src/Components/Cryptocurrencies.jsx
+++ src/Components/Cryptocurrencies.jsx
@@ -37,7 +37,7 @@ const Cryptocurrencies = ({simplified}) => {
           <Link to={`/crypto/${currency.uuid}` }>
             
             <Card title={`${currency.rank} , ${currency.name} `}
-            extra={<img className='crypto-image' src={currency.iconUrl}/>}
+            extra={<img className='crypto-image' src={currency.iconUrl} alt='No Way....'/>}
             hoverable
             >
               <p>Price:{millify(currency.price)}</p>
